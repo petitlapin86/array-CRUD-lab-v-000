2c8914f0cc7979df586156044e1c0488696de37a
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-01 19:20:19 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ create_an_array = ["have", "yourself", "a", "merry"]
 end
 
 def add_element_to_end_of_array(array, element)
-array = [] element << "Christmas"
+array = [] 
 end
 
 def add_element_to_start_of_array(array, element)
